e6e9c661e5e54c81fb165047d6beea135ce01403
Kindaglia <matteodrago007@gmail.com>
2023-02-08 17:28:37 +0100

    add utente azienda

---
+++ java_spring_table_jpa/demo/src/main/java/com/example/demo/controller/UtenteAziendaController.java
@@ -0,0 +1,39 @@
+package com.example.demo.controller;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.example.demo.Utente_azienda;
+import com.example.demo.repository.Utente_aziendaRepo;
+
+@RestController
+public class UtenteAziendaController {
+
+    @Autowired
+    private Utente_aziendaRepo utente_aziendaRepo;
+
+    // chiamaete get, tutti gli utenti
+    @GetMapping("/utentiAziende")
+    public List<Utente_azienda> getUtentiAziende(){
+        return utente_aziendaRepo.findAll();
+    }
+
+    //il path in {} non è a caso 
+    @GetMapping("/utentiAzienda/{UtenteAziendaId}")
+    public Utente_azienda getUtenteAzienda(@PathVariable Integer UtenteAziendaId){
+        return utente_aziendaRepo.findById(UtenteAziendaId).orElseThrow();
+      
+    }
+
+    /// chiamate post 
+    @PostMapping("/utentiAziende")
+    public Utente_azienda createUtentiAzienda(@RequestBody Utente_azienda newUtentiAzienda){
+        return utente_aziendaRepo.save(newUtentiAzienda);
+    }
+}
--- java_spring_table_jpa/demo/src/main/java/com/example/demo/repository/Utente_aziendaRepo.java
+++ java_spring_table_jpa/demo/src/main/java/com/example/demo/repository/Utente_aziendaRepo.java
@@ -1,5 +1,6 @@
 package com.example.demo.repository;
 
+import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.JpaSpecificationExecutor;
 import org.springframework.data.repository.CrudRepository;
 import org.springframework.stereotype.Repository;
@@ -8,7 +9,6 @@ import com.example.demo.Doc;
 import com.example.demo.Utente_azienda;
 
 @Repository
-public interface Utente_aziendaRepo
- extends CrudRepository<Utente_azienda, Integer>, JpaSpecificationExecutor<Utente_azienda> {
+public interface Utente_aziendaRepo  extends JpaRepository<Utente_azienda, Integer> {
 
 }
